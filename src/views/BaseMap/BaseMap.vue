17b66d1226a2b22f04efa6ca69530aa251d60bec
wangyw <wang179660>
2021-02-02 11:32:26 +0800

    basemap2

--- src/views/BaseMap/BaseMap.vue
+++ src/views/BaseMap/BaseMap.vue
@@ -45,19 +45,26 @@ export default {
                     url: 'https://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
                 })
             );
+            // viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
+            // 	url: "http://webst02.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8",
+            // 	layer: "tdtAnnoLayer",
+            // 	style: "default",
+            // 	format: "image/jpeg",
+            // 	tileMatrixSetID: "GoogleMapsCompatible"
+            // }));
         },
         addMapboxMap() {
-            viewer.scene.imageryLayers.addImageryProvider(
-                new Cesium.WebMapTileServiceImageryProvider({
-                    url:
-                        'http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=ebf64362215c081f8317203220f133eb',
-                    layer: 'tdtBasicLayer',
-                    style: 'default',
-                    format: 'image/jpeg',
-                    tileMatrixSetID: 'GoogleMapsCompatible',
-                    show: false,
-                })
-            );
+            // viewer.scene.imageryLayers.addImageryProvider(
+            //     new Cesium.WebMapTileServiceImageryProvider({
+            //         url:
+            //             'http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=ebf64362215c081f8317203220f133eb',
+            //         layer: 'tdtBasicLayer',
+            //         style: 'default',
+            //         format: 'image/jpeg',
+            //         tileMatrixSetID: 'GoogleMapsCompatible',
+            //         show: false,
+            //     })
+            // );
         },
     },
 };
